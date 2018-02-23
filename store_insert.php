e792da4bd37178724a2148c6b7bcd71f911fa810
abdallah.sarayrah <abdallah.er11@gmail.com>
2018-02-23 14:33:18 +0200

    تم اضافة رقم هاتف المرتبط بالمحل على الكود ليضاف لقاعدة البيانات

--- store_insert.php
+++ store_insert.php
@@ -6,7 +6,7 @@ mysqli_query($con, "SET NAMES 'utf8'");
 mysqli_query($con, 'SET CHARACTER SET utf8');
 
 $statement = $con->prepare("insert into store(store_name, area_id, complex_id, "
-        . "store_image) values(?, ?, ?, ?)");
-$statement->bind_param("siis", $_GET['store_name'], $_GET['area_id'], 
-        $_GET['complex_id'], $_GET['store_image']);
+        . "store_image, mobile) values(?, ?, ?, ?, ?)");
+$statement->bind_param("siiss", $_GET['store_name'], $_GET['area_id'], 
+        $_GET['complex_id'], $_GET['store_image'], $_GET['mobile']);
 $statement->execute();
