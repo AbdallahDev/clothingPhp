2d19af14877af90c8a843ebf783579473dc654df
abdallah.sarayrah <abdallah.er11@gmail.com>
2018-02-23 17:50:34 +0200

    i added the file store_clothing_section in BLL folder to do all the CRUD methods on the store_clothing_section db table

---
+++ BLL/store_clothing_section.php
@@ -0,0 +1,15 @@
+<?php
+
+include_once '../DAL/my_db.php';
+
+class store extends my_db {
+    
+    public function store_insert($store_name, $area_id, $complex_id, 
+            $store_image, $mobile) {
+        $this->mod_data('insert into store(store_name, area_id, complex_id, '
+        . 'store_image, mobile) values(?, ?, ?, ?, ?)', 'siiss', 
+                array(&$store_name, &$area_id, &$complex_id, &$store_image, 
+                    &$mobile));
+    }
+    
+}
