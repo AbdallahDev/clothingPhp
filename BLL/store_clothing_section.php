9eb273b4a864c612de48c730a6b5bd7692d85578
abdallah.sarayrah <abdallah.er11@gmail.com>
2018-02-23 18:13:16 +0200

    i've edited the method store_clothing_section_insert to be suitable to to insert in the db table store_clothing_section

--- BLL/store_clothing_section.php
+++ BLL/store_clothing_section.php
@@ -4,12 +4,11 @@ include_once '../DAL/my_db.php';
 
 class store_clothing_section extends my_db {
 
-    public function store_clothing_section_insert($store_name, $area_id
-    , $complex_id, $store_image, $mobile) {
-        $this->mod_data('insert into store(store_name, area_id, complex_id, '
-                . 'store_image, mobile) values(?, ?, ?, ?, ?)', 'siiss'
-                , array(&$store_name, &$area_id, &$complex_id, &$store_image,
-            &$mobile));
+    public function store_clothing_section_insert($store_id
+    , $clothing_section_id) {
+        $this->mod_data('insert into store_clothing_section(store_id, '
+                . 'clothing_section_id) values(?, ?)', 'ii'
+                , array(&$store_id, &$clothing_section_id));
     }
 
 }
