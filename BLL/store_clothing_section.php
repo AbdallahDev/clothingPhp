e56aa466e1efcefa927ac603c33755a19977bf0a
abdallah.sarayrah <abdallah.er11@gmail.com>
2018-02-23 18:01:40 +0200

    changed the method name to store_clothing_section_insert

--- BLL/store_clothing_section.php
+++ BLL/store_clothing_section.php
@@ -4,7 +4,7 @@ include_once '../DAL/my_db.php';
 
 class store_clothing_section extends my_db {
     
-    public function store_insert($store_name, $area_id, $complex_id, 
+    public function store_clothing_section_insert($store_name, $area_id, $complex_id, 
             $store_image, $mobile) {
         $this->mod_data('insert into store(store_name, area_id, complex_id, '
         . 'store_image, mobile) values(?, ?, ?, ?, ?)', 'siiss', 
