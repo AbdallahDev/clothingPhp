92dc5786269cb263dd4d1de074929e5126f71c4d
abdallah.sarayrah <abdallah.er11@gmail.com>
2018-02-23 18:00:31 +0200

    changed the class name to store_clothing_section, because the file is copied from the store file

--- BLL/store_clothing_section.php
+++ BLL/store_clothing_section.php
@@ -2,7 +2,7 @@
 
 include_once '../DAL/my_db.php';
 
-class store extends my_db {
+class store_clothing_section extends my_db {
     
     public function store_insert($store_name, $area_id, $complex_id, 
             $store_image, $mobile) {
