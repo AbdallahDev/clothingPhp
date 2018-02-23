04f0908072497eb6752adb801f5188aa6109f25d
abdallah.sarayrah <abdallah.er11@gmail.com>
2018-02-23 19:23:01 +0200

    i created the store_id variable to store the id of the inserted store by selecting the max id of the stores in the store table and by increasing it by one to make it the id of the next inserted store

--- PRL/store_insert.php
+++ PRL/store_insert.php
@@ -2,11 +2,20 @@
 
 include_once '../BLL/store.php';
 
+//i declared object from the store class to be used later.
+$store = new store();
+
 //here i'll check for the complex value, because some stores don't exist in 
 //complexes, so in that case i'll set the complex id to 0, that means in the 
 //database it dosen't belong to any complex.
 $complex_id = 0; //this is the default value for the complex id
-//bellow i declared all the variable that will be inserted in the store table
+//
+//for the sotre id i'll save the max store id in the database plus 1
+//to make sure that all the id's aren't identical, because it's the primary key,
+//and i can't make it autoincreament because i'll needed later.
+$store_id = $store->store_max_id_select() + 1;
+
+//bellow i declared all the reset variable that will be inserted in the store table
 //from the get method
 $store_name = $_GET['store_name'];
 $area_id = $_GET['area_id'];
@@ -21,5 +30,4 @@ if (isset($_GET['complex_id']) && $_GET['complex_id'] != 0) {
     $complex_id = $_GET['complex_id'];
 }
 
-$store = new store();
-$store->store_insert($store_name, $area_id, $complex_id, $store_image, $mobile);
+$store->store_insert($store_id, $store_name, $area_id, $complex_id, $store_image, $mobile);
