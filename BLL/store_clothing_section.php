3f01344689360852877655493407ad5b5bb3979d
abdallah.sarayrah <abdallah.er11@gmail.com>
2018-02-24 19:12:19 +0200

    i've done all the functions and methods to add store section types in the store_clothing_section db table.

--- BLL/store_clothing_section.php
+++ BLL/store_clothing_section.php
@@ -3,7 +3,20 @@
 include_once '../DAL/my_db.php';
 
 class store_clothing_section extends my_db {
-    
+
+    function delete_all_store_clothing_section($store_id) {
+        $this->mod_data('delete from store_clothing_section where store_id = ?'
+                , 'i', array(&$store_id));
+    }
+
+    public function check_if_not_exist($store_id, $clothing_section_id) {
+        $result = $this->get_data('select * from store_clothing_section '
+                . 'where store_id = ? and clothing_section_id = ?', 'ii'
+                , array(&$store_id, &$clothing_section_id));
+        if ($result->num_rows == 0)
+            return TRUE;
+    }
+
     public function store_clothing_section_insert($store_id
     , $clothing_section_id) {
         $this->mod_data('insert into store_clothing_section(store_id, '
@@ -12,4 +25,3 @@ class store_clothing_section extends my_db {
     }
 
 }
-
--- PRL/test.php
+++ PRL/test.php
@@ -1,11 +1,25 @@
 <?php
 
-include_once '../BLL/store.php';
+include_once '../BLL/store_clothing_section.php';
+$store_clothing_section = new store_clothing_section();
 
-//i declared object from the store class to be used later.
-$store = new store();
-$max_id_rs = $store->store_max_id_select();
-$max_store_id = $max_id_rs->fetch_assoc();
+$store_clothing_section->delete_all_store_clothing_section($_GET['store_id']);
 
-
-echo $max_store_id['store_id'] + 1;
+if (isset($_GET['men']) &&
+        $store_clothing_section->check_if_not_exist($_GET['store_id']
+                , $_GET['men']) == TRUE) {
+    $store_clothing_section->store_clothing_section_insert($_GET['store_id']
+            , $_GET['men']);
+}
+if (isset($_GET['women']) && $_GET['women'] == 2 &&
+        $store_clothing_section->check_if_not_exist($_GET['store_id']
+                , $_GET['women']) == TRUE) {
+    $store_clothing_section->store_clothing_section_insert($_GET['store_id']
+            , $_GET['women']);
+}
+if (isset($_GET['children']) && $_GET['children'] == 3 &&
+        $store_clothing_section->check_if_not_exist($_GET['store_id']
+                , $_GET['children']) == TRUE) {
+    $store_clothing_section->store_clothing_section_insert($_GET['store_id']
+            , $_GET['children']);
+}
