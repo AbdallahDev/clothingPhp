2c6e8506fe53daa1e4b98746a95034d26dc369d1
abdallah.sarayrah <abdallah.er11@gmail.com>
2018-03-03 17:43:34 +0200

    did the code to select governorates from the db

---
+++ PRL/governorates_select.php
@@ -0,0 +1,14 @@
+<?php
+
+include_once '../BLL/governorate.php';
+
+//i declared object from the governorate class to be used later.
+$governorate = new governorate();
+
+$rs = $governorate->governorate_select_all();
+$array = array(); //this array to store the results from the db
+while ($row = $rs->fetch_assoc()) {
+    array_push($array, $row);
+}
+
+echo json_encode($array);
