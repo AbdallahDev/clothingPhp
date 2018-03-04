113f6e7df65caea213585eeb50c783b1217a2e98
abdallah.sarayrah <abdallah.er11@gmail.com>
2018-03-04 16:33:31 +0200

    commit

--- BLL/area.php
+++ BLL/area.php
@@ -4,7 +4,7 @@ include_once '../DAL/my_db.php';
 
 class area extends my_db {
 
-    //here by this function i'll select all the governorates from the db
+    //here by this function i'll select all the areas from the db
     public function governorate_select_all() {
         return $this->get_all_data('SELECT * FROM `governorate`');
     }
