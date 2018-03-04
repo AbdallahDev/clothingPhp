a5d64f9c5cf05d3f6e4017ce3306c08ef1255336
abdallah.sarayrah <abdallah.er11@gmail.com>
2018-03-04 16:39:09 +0200

    select all areas from db function

--- BLL/area.php
+++ BLL/area.php
@@ -5,8 +5,8 @@ include_once '../DAL/my_db.php';
 class area extends my_db {
 
     //here by this function i'll select all the areas from the db
-    public function governorate_select_all() {
-        return $this->get_all_data('SELECT * FROM `governorate`');
+    public function area_select_all() {
+        return $this->get_all_data('SELECT `area_id`, `area_name` FROM `area`');
     }
 
 }
