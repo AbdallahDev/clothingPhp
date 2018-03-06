a150e28ffc4245318bac3ce81a55412f3fe40591
abdallah.sarayrah <abdallah.er11@gmail.com>
2018-03-06 13:36:34 +0200

    commit

--- BLL/area.php
+++ BLL/area.php
@@ -7,7 +7,8 @@ class area extends my_db {
     //here by this function i'll select all the areas from the db based on the 
     //governorate id
     public function area_select_governorate_id($governorate_id) {
-        return $this->get_data("SELECT * FROM `area` WHERE governorate_id = ?"
+        return $this->get_data("SELECT `area_id`, `area_name` FROM `area` WHERE "
+                        . "`governorate_id` = ?"
                         , "i", array(&$governorate_id));
     }
 
