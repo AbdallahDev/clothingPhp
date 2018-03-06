33cf86e4158ad598ed669d5cdf3e1888a9a54e70
abdallah.sarayrah <abdallah.er11@gmail.com>
2018-03-06 13:08:19 +0200

    commit

--- PRL/areas_select.php
+++ PRL/areas_select.php
@@ -5,7 +5,7 @@ include_once '../BLL/area.php';
 //i declared object from the area class to be used later.
 $area = new area();
 
-$rs = $area->area_select_all();
+$rs = $area->area_select_governorate_id($_GET['governorate_id']);
 $array = array(); //this array to store the results from the db
 while ($row = $rs->fetch_assoc()) {
     array_push($array, $row);
