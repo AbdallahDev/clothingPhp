db386322edba790d63092a6ce463119bc2c04e19
abdallah.sarayrah <abdallah.er11@gmail.com>
2018-03-06 13:04:57 +0200

    select the areas based on the governorate id

--- BLL/area.php
+++ BLL/area.php
@@ -4,9 +4,11 @@ include_once '../DAL/my_db.php';
 
 class area extends my_db {
 
-    //here by this function i'll select all the areas from the db
-    public function area_select_all() {
-        return $this->get_all_data('SELECT `area_id`, `area_name` FROM `area`');
+    //here by this function i'll select all the areas from the db based on the 
+    //governorate id
+    public function area_select_governorate_id($governorate_id) {
+        return $this->get_data("SELECT * FROM `area` WHERE governorate_id = ?"
+                        , "i", array(&$governorate_id));
     }
 
 }
