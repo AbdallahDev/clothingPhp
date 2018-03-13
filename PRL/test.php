04f61f9f7e57a3e7859d0b3f38a38df60e461c76
abdallah.sarayrah <abdallah.er11@gmail.com>
2018-03-13 19:00:41 +0200

    commit

--- PRL/test.php
+++ PRL/test.php
@@ -1,25 +1,3 @@
 <?php
 
-include_once '../BLL/store_clothing_section.php';
-$store_clothing_section = new store_clothing_section();
-
-$store_clothing_section->delete_all_store_clothing_section($_GET['store_id']);
-
-if (isset($_GET['men']) &&
-        $store_clothing_section->check_if_not_exist($_GET['store_id']
-                , $_GET['men']) == TRUE) {
-    $store_clothing_section->store_clothing_section_insert($_GET['store_id']
-            , $_GET['men']);
-}
-if (isset($_GET['women']) && $_GET['women'] == 2 &&
-        $store_clothing_section->check_if_not_exist($_GET['store_id']
-                , $_GET['women']) == TRUE) {
-    $store_clothing_section->store_clothing_section_insert($_GET['store_id']
-            , $_GET['women']);
-}
-if (isset($_GET['children']) && $_GET['children'] == 3 &&
-        $store_clothing_section->check_if_not_exist($_GET['store_id']
-                , $_GET['children']) == TRUE) {
-    $store_clothing_section->store_clothing_section_insert($_GET['store_id']
-            , $_GET['children']);
-}
+move_uploaded_file($_FILES['pic']['tmp_name'], "../imgs/" . $_FILES['pic']['name']);
--- PRL/test1.php
+++ PRL/test1.php
@@ -1,15 +1,3 @@
 <?php
 
-include_once '../BLL/store_clothing_section.php';
-$store_clothing_section = new store_clothing_section();
-
-if (isset($_GET['men']) &&
-        $store_clothing_section->check_if_not_exist($_GET['store_id']
-                , $_GET['men']) == 1) {
-    $store_clothing_section->store_clothing_section_insert($_GET['store_id']
-            , $_GET['men']);
-} elseif (!isset($_GET['men']) &&
-        $store_clothing_section->check_if_not_exist($_GET['store_id']
-                , $_GET['men']) != 1) {
-    
-}
+echo $_GET['i']
